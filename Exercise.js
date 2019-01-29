4b7c8cba440c91391919db3a966bac607e7c9200
ArminKaradza <47142681+ArminKaradza@users.noreply.github.com>
2019-01-29 20:28:09 +0100

    Update Exercise.js

--- Exercise.js
+++ Exercise.js
@@ -29,3 +29,10 @@ function multiplication()
 		document.getElementById("mult-tb").innerHTML += res+"<br/>";
 	}
 }
+function openBrowser(url)
+{
+	//var url = 'https://cordova.apache.org';
+	var target = '_blank';
+	var options = "location = yes"
+	var ref = cordova.InAppBrowser.open(url, target, options);
+}
