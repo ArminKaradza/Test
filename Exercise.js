9edfd4273a7bd7a69b4f263c585b052796775df0
ArminKaradza <47142681+ArminKaradza@users.noreply.github.com>
2019-01-29 13:06:54 +0100

    Nødvendige filer placeret

---
+++ Exercise.js
@@ -0,0 +1,31 @@
+function insertText()
+{
+	document.getElementById("newContent").innerHTML = "Some new text: <br> Hello <br>";
+}
+function calculate()
+{
+	var t = widthBox.value;
+	//read from the textBoxs
+	var w = document.getElementById("widthBox").value
+	var l = document.getElementById("lenghtBox").value
+	var h = document.getElementById("heightBox").value
+	//Convert to Float
+	w = parseFloat(w);
+	l = parseFloat(l);
+	h = parseFloat(h);
+	var result = w*l*h;
+	//show result in div
+	document.getElementById("result").innerHTML = result;
+}
+function multiplication()
+{
+	//read from textBox
+	document.getElementById("mult-tb").innerHTML ="";
+	var f = document.getElementById("factor").value;
+	for(var i =1;i<=20;i++)
+	{
+		var res = f*i;
+		//show result in div
+		document.getElementById("mult-tb").innerHTML += res+"<br/>";
+	}
+}
